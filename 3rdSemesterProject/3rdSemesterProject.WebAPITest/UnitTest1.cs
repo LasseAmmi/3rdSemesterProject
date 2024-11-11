870ad689670b4fa9b1e34effe5acc3ca171fd1bd
LasseAmmi <149362756+LasseAmmi@users.noreply.github.com>
2024-11-11 09:09:54 +0100

    Update til main
    Update to main layout after help consulting with Lars

---
+++ 3rdSemesterProject/3rdSemesterProject.WebAPITest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace _3rdSemesterProject.WebAPITest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 3rdSemesterProject/3rdSemesterProject.WinFormTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace _3rdSemesterProject.WinFormTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
